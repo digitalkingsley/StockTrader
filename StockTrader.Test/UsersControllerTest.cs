875b5fffb7f4ae42c2d59fcc7b28d23636cb4da8
Kingsley Okei <kingsley.okei@ekedp.com>
2019-10-12 09:26:56 +0100

    Modify Test Project Structure to allow for proper testing

--- StockTrader.Test/UsersControllerTest.cs
+++ StockTrader.Test/UsersControllerTest.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using System.Threading.Tasks;
+using Moq;
+using StockTrader.StockTrader_Model;
+using StockTrader.Service.InfrastructureService;
+using StockTrader.Controllers;
 
 namespace StockTrader.Test
 {
     public class UsersControllerTest
     {
         [Fact]
-        public void Test1()
+        public async Task Register_User_With_Moq()
         {
-
+           
         }
+
     }
 }
